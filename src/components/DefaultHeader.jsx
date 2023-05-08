ae485db37c7be4991a48946688e9733d140f37bc
haenanim121@gmail.com <haenanim121@gmail.com>
2023-05-08 20:43:03 +0900

    FIX : 카드 이미지 없을 떄 형식 추가

--- src/components/DefaultHeader.jsx
+++ src/components/DefaultHeader.jsx
@@ -9,6 +9,7 @@ function DefaultHeader() {
         </div>
         <div className="right_side">
           <button>글쓰기</button>
+          <a href="/posting">글쓰기</a>
           <input className="search_box" type="text" placeholder="찾기" />
         </div>
       </div>
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,12 +1,17 @@
 import './PostItem.css';
 
 export default function PostItem({ post }) {
+  console.log(post);
   return (
     <div className="item">
       <div key={`post-list-${post.id}`} style={{ width: '100%' }}>
         <div className="title">{post.title}</div>
-        <img className="image" src={post.imgUrl} />
-        <div className="content">{post.content}</div>
+        {post.img_name ? (
+          <img className="image" src={post.imgUrl} />
+        ) : (
+          <div></div>
+        )}
+        <div className="content">{post.created_at}</div>
       </div>
     </div>
   );
